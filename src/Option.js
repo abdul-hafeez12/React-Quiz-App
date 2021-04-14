ecfe1b3ead42ef09b1992057a70660f4b72b0112
Abdul Hafeez <79803666+abdul-hafeez12@users.noreply.github.com>
2021-04-14 13:50:25 +0500

    v 1.1

--- src/Option.js
+++ src/Option.js
@@ -1,17 +1,34 @@
-
-import React, { useState } from  'react'
+import React, { useState ,useEffect} from  'react'
 import Form from 'react-bootstrap/Form';
 
-function Options({text}) {
+function Options(props) {
+
+
+   const onChange = (e) => {
+       if(e.target.value===props.w[props.y].answer){
+        props.w[props.y].score=1;
+        props.w[props.y].status="correct";
+       }
+
+       else{
+        props.w[props.y].status="incorrect";
+       }
+       
+      
+        
+      }
 
 return(
+
     <div className="border border-info mb-2 p-1 w-25">
          
         <Form.Check
           type="radio"
-          label={text}
+          value={props.text}
+          label={props.text}
           name="formHorizontalRadios"
           id="formHorizontalRadios1"
+          onChange={onChange.bind(this)}
            />
   
     </div>
